26a1fdae2556b22f0007d6164132b9216179c21b
leehw1011 <leehw1011@naver.com>
2021-05-27 19:27:34 +0900

    .

--- project4/person.c
+++ project4/person.c
@@ -18,11 +18,33 @@
 // 헤더 레코드의 메타데이터를 저장하거나 수정하는 경우 페이지 단위로 처리하지 않고 직접 레코드 파일을 접근해서 처리한다.
  
 //
+// 헤더 영역의 내용을 갱신한다.
+//
+void headerUpdate(FILE *fp, int total_page, int total_record, int last_pagenum, int last_recordnum)
+{
+	char headerRecord[16];
+	memset(headerRecord,(char)0xFF,16);
+	memcpy(headerRecord,&total_page,4);
+	memcpy(headerRecord+4,&total_record,4);
+	memcpy(headerRecord+8,&last_pagenum,4);
+	memcpy(headerRecord+12,&last_recordnum,4);
+
+	fseek(fp,0,SEEK_SET);
+	fwrite(headerRecord,16,1,fp);
+}
+
+//
 // 페이지 번호에 해당하는 페이지를 주어진 페이지 버퍼에 읽어서 저장한다. 페이지 버퍼는 반드시 페이지 크기와 일치해야 한다.
+//
 //
 void readPage(FILE *fp, char *pagebuf, int pagenum)
 {
- 
+	if(fseek(fp,16+PAGE_SIZE*pagenum,SEEK_SET)<0){
+		printf("fseek error\n");
+	}
+	if(fread(pagebuf,PAGE_SIZE,1,fp)<0){
+		printf("fread error\n");
+	}
 }
  
 //
@@ -31,7 +53,13 @@ void readPage(FILE *fp, char *pagebuf, int pagenum)
 //
 void writePage(FILE *fp, const char *pagebuf, int pagenum)
 {
- 
+	if(fseek(fp,16+PAGE_SIZE*pagenum,SEEK_SET)<0){
+		printf("fseek error\n");
+	}
+	if(fwrite(pagebuf,PAGE_SIZE,1,fp)<0){
+		printf("fwrite error\n");
+	}
+
 }
  
 //
@@ -48,7 +76,7 @@ void pack(char *recordbuf, const Person *p)
 //
 void unpack(const char *recordbuf, Person *p)
 {
- 
+
 }
  
 //
@@ -56,11 +84,78 @@ void unpack(const char *recordbuf, Person *p)
 //
 void add(FILE *fp, const Person *p)
 {
-	//이 안에서 pack()호출?
 	char recordbuf[MAX_RECORD_SIZE];
+	char pagebuf[PAGE_SIZE];
+	int total_page, total_record, last_pagenum, last_recordnum;
+	int record_length;
+	char headbuf[16];
 
+	//구조체의 정보를 레코드파일에 저장할 형태로 packing
 	memset(recordbuf,(char)0xFF,MAX_RECORD_SIZE);
 	pack(recordbuf,p);
+	record_length = strlen(recordbuf);
+
+	//헤더 레코드 내용 읽어오기
+	fseek(fp,0,SEEK_SET);
+	fread(headbuf,16,1,fp);
+	memcpy(&total_page,headbuf,4);	//전체 데이터 페이지 수
+	memcpy(&total_record,headbuf+4,4);	//레코드 파일에 존재하는 모든 레코드의 수
+	memcpy(&last_pagenum,headbuf+8,4);	//가장 최근 삭제된 레코드의 페이지 번호
+	memcpy(&last_recordnum,headbuf+12,4);	//가장 최근 삭제된 레코드 번호
+
+	//삭제 레코드가 없는 경우
+	if(last_pagenum==-1&&last_recordnum==-1){
+		printf("삭제 레코드가 없는 경우\n");
+		//최초의 저장인 경우
+		printf("total_record : %d\n",total_record);
+		if(total_record==0){
+			memset(pagebuf,(char)0xFF,PAGE_SIZE);
+			memcpy(pagebuf,recordbuf,record_length);
+			writePage(fp,pagebuf,1);
+			// 페이지 수, 레코드 수 증가
+			headerUpdate(fp,total_page+1,total_record+1,last_pagenum,last_recordnum);
+		}
+
+		//최초의 저장이 아니라면
+		else{
+			//마지막 페이지에 레코드를 저장할 수 있는지 확인하자.
+			memset(pagebuf,(char)0xFF,PAGE_SIZE);
+			readPage(fp,pagebuf,total_page-1);
+			int len;
+			int recordnum, total_len=0;	//페이지에 저장되어 있는 레코드의 수, 사용중인 공간의 총 길이
+			memcpy(&recordnum,pagebuf,4);
+			printf("recordnum : %d\n",recordnum);
+			for(int i=0;i<recordnum;i++){
+				memcpy(&len,pagebuf+4+8*(i+1),4);
+				total_len+=len;
+			}
+
+			// 페이지에 남은 공간이 저장하려는 레코드보다 크고 & 헤더 영역에도 남은 공간이 있어야함
+			if(DATA_AREA_SIZE-total_len>=record_length && HEADER_AREA_SIZE-(4+8*recordnum)>=8){
+				printf("여기까지 왔나요?\n");
+				memset(pagebuf,(char)0xFF,PAGE_SIZE);
+				readPage(fp,pagebuf,total_page-1);
+
+				//왜 앞쪽으로 저장이 되는거지 ?????
+				memcpy(pagebuf+HEADER_AREA_SIZE+total_len,recordbuf,record_length);
+				writePage(fp,pagebuf,total_page-1);
+				// 레코드 수 1 증가
+				headerUpdate(fp,total_page,total_record+1,last_pagenum,last_recordnum);
+
+			}
+
+			else{
+				// 새로 페이지 할당
+			}
+
+		}
+	}
+
+	//삭제된 레코드가 있는 경우
+	else{
+		printf("삭제 레코드가 있는 경우\n");
+	
+	}
 }
  
 //
